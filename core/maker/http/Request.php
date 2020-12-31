77e651326f98005a75cf50381e4bcb702e41c426
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-12-31 19:14:21 -0300

    refactor: use trait Client IP

--- core/maker/http/Request.php
+++ core/maker/http/Request.php
@@ -4,12 +4,14 @@ namespace Core\maker\http;
 
 use Core\maker\http\traits\Body;
 use Core\maker\http\traits\Query;
+use Core\maker\http\traits\ClientIP;
 use stdClass;
 
 class Request
 {
     use Body;
     use Query;
+    use ClientIP;
 
     public function __construct()
     {
@@ -21,6 +23,7 @@ class Request
         $maker = new stdClass();
         $maker->body = $this->createBody();
         $maker->query = $this->createQuery();
+        $maker->clientIp = $this->loadClientIP();
         return $maker;
     }
 }
