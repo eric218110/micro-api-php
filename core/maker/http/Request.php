3a37f9599340b42697de6312528fb8baa34211d7
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-12-31 18:20:16 -0300

    feat: create traits Body

--- core/maker/http/Request.php
+++ core/maker/http/Request.php
@@ -2,43 +2,24 @@
 
 namespace Core\maker\http;
 
+use Core\maker\http\traits\Body;
 use stdClass;
 
 class Request
 {
+    use Body;
 
-    public function __construct() {
+    public function __construct()
+    {
         return $this->maker();
     }
 
-    public function loadBody()
-    {
-        return $this->createBody();
-    }
+    
 
     private function maker()
     {
         $maker = new stdClass();
         $maker->body = $this->createBody();
         return $maker;
-    }
-
-    private function createBody()
-    {
-        $body = file_get_contents('php://input');
-        if ($body) {
-            $obj = new stdClass;
-            $bodyToArray = json_decode($body, true);
-            foreach ($bodyToArray as $key => $value) {
-                if ($key && $value) {
-                    $obj->$key = $value;
-                }
-            }
-            $body = $obj;
-        }
-        if($body === null){
-            return [];
-        }
-        return $body;
     }
 }
