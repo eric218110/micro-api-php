4fc864055f97bd291c996bca84796b987001a269
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-12-31 18:59:02 -0300

    refactor: use trait Query

--- core/maker/http/Request.php
+++ core/maker/http/Request.php
@@ -3,23 +3,24 @@
 namespace Core\maker\http;
 
 use Core\maker\http\traits\Body;
+use Core\maker\http\traits\Query;
 use stdClass;
 
 class Request
 {
     use Body;
+    use Query;
 
     public function __construct()
     {
         return $this->maker();
     }
 
-    
-
     private function maker()
     {
         $maker = new stdClass();
         $maker->body = $this->createBody();
+        $maker->query = $this->createQuery();
         return $maker;
     }
 }
