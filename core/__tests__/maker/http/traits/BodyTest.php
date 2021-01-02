836b3b9f31073f0e95d89eb2bbb1586f1f47fdb2
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-02 13:02:19 -0300

    feat: create test for trait Body

---
+++ core/__tests__/maker/http/traits/BodyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase as PHPUnit;
+
+class BodyTest extends PHPUnit
+{
+    public function testShouldCreateBodyInRequest()
+    {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+}
