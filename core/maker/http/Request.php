60a0bf75026bb4f0f1c5bf4fc5a8fa51ed6b4f52
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-03 11:28:12 -0300

    refactor: use traits Params

--- core/maker/http/Request.php
+++ core/maker/http/Request.php
@@ -5,6 +5,7 @@ namespace Core\maker\http;
 use Core\maker\http\traits\Body;
 use Core\maker\http\traits\Query;
 use Core\maker\http\traits\ClientIP;
+use Core\maker\http\traits\Params;
 use stdClass;
 
 class Request
@@ -12,16 +13,20 @@ class Request
     use Body;
     use ClientIP;
     use Query;
+    use Params;
 
-    public function createRequest()
+    public function createRequest(string $currentRoute)
     {
-        return $this->maker();
+        return $this->maker($currentRoute);
     }
 
-    private function maker()
+    private function maker(string $currentRoute)
     {
         $maker = new stdClass();
         $maker->body = $this->createBody();
+        $maker->clientIP = $this->loadClientIP();
+        $maker->params = $this->loadParamsRequest($currentRoute);
+        
         return $maker;
     }
 }
