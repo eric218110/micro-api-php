c0b31f5c92e6eb89cae8f471e0cc12da6d1edef5
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-03 07:33:55 -0300

    test: should return trait ClientIP returns address ip valid if IP no informed

--- __tests__/maker/http/traits/TestTraitClientIp.php
+++ __tests__/maker/http/traits/TestTraitClientIp.php
@@ -1,7 +1,25 @@
-<?php 
+<?php
 
+use Core\maker\http\Request;
+use CoreTest\maker\http\fakes\FakeClientHTTP;
 use PHPUnit\Framework\TestCase;
 
-class TestTraitClientIp implements TestCase {
+class TestTraitClientIp extends TestCase
+{
+    public function testShouldTraitClientIPReturnIPIfNoInformed()
+    {
 
+        // ARRANGE
+        $client = FakeClientHTTP::loadClientHTTP();
+        $client->request('GET', '/');
+        $noIpAddress = '0.0.0.0';
+        $request = new Request();
+        $request->createRequest();
+
+        // ACT
+        $ipAddress = $request->loadClientIP();
+
+        // ASSERT
+        $this->assertEquals($noIpAddress, $ipAddress);
+    }
 }
