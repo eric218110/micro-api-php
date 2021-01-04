19ebe2a647977786c4c561b65489039a0af7ade3
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-04 12:38:17 -0300

    refactor: create function isRootDir

--- core/utils/path/Path.php
+++ core/utils/path/Path.php
@@ -2,6 +2,9 @@
 
 namespace Core\utils\path;
 
+use Core\maker\server\Server;
+use Core\maker\utils\StringUtil;
+
 class Path
 {
 
@@ -14,5 +17,21 @@ class Path
     {
         $pathRoutes = '/src/main/routes/routes.php';
         return self::loadBaseIndexPath() . $pathRoutes;
+    }
+
+    public static function loadBaseFolder()
+    {
+        $baseDir = self::loadBaseIndexPath();
+        $explodeDir = explode('/', $baseDir);
+        return end($explodeDir);
+    }
+
+    public static function isRootDir()
+    {
+        $hostname = Server::loadHostname();
+        $uri = StringUtil::removeFirstAndLastBar(parse_url(Server::loadRequestURI(), PHP_URL_PATH));
+
+        if ($hostname === Path::loadBaseFolder()) return true;
+        if ($uri == Path::loadBaseFolder()) return false;
     }
 }
