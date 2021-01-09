e2ff68f4ad5dee35c11f4bb8281957e0e66e9c1c
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-09 10:30:46 -0300

    feat: create FactoryPath for maker Path class

---
+++ core/main/factories/path/FactoryPath.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\main\factories\path;
+
+use Core\data\useCases\server\request\ServerRequest;
+use Core\data\useCases\utils\path\Path;
+use Core\data\useCases\utils\strings\StringsUtil;
+use Core\main\factories\protocols\FactoryMachine;
+
+class FactoryPath extends FactoryMachine
+{
+
+    private $stringsUtils;
+    private $serverRequest;
+
+    public function __construct()
+
+    {
+        $this->stringsUtils = new StringsUtil();
+        $this->serverRequest = new ServerRequest();
+    }
+
+    public function maker(): Path{
+        return new Path($this->serverRequest, $this->serverRequest, $this->stringsUtils);
+    }
+
+}
