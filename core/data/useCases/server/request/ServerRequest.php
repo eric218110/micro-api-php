6baec91690c124e5d9b1ef1e26597b3f467beffa
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-11 16:31:36 -0300

    refactor: implements ServerRequestMethod interface

--- core/data/useCases/server/request/ServerRequest.php
+++ core/data/useCases/server/request/ServerRequest.php
@@ -3,17 +3,23 @@
 namespace Core\data\useCases\server\request;
 
 use Core\domain\protocols\server\request\ServerRequestHostName;
+use Core\domain\protocols\server\request\ServerRequestMethod;
 use Core\domain\protocols\server\request\ServerRequestURI;
 
-class ServerRequest implements ServerRequestHostName, ServerRequestURI
+class ServerRequest implements ServerRequestHostName, ServerRequestURI, ServerRequestMethod
 {
     public function loadRequestURI(): string
     {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);;
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
     public function loadHostname(): string
     {
         return $_SERVER['HTTP_HOST'];
+    }
+
+    public function loadRequestMethod(): string
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
     }
 }
