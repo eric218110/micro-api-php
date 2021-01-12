334579a3d5de7da8aae7b426f4766fa34ab5365e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-12 12:28:03 -0300

    refactor: change make function to a create Request

--- core/main/factories/http/request/create/FactoryCreateRequest.php
+++ core/main/factories/http/request/create/FactoryCreateRequest.php
@@ -6,11 +6,31 @@ namespace Core\main\factories\http\request\create;
 
 use Core\data\useCases\http\request\create\CreateRequest;
 use Core\data\useCases\server\php\contents\PHPServer;
+use Core\data\useCases\server\http\load\ServerHTTPLoad;
+use Core\data\useCases\server\http\validate\ServerHTTPValidate;
+use Core\main\factories\protocols\FactoryMachine;
 
-class FactoryCreateRequest extends \Core\main\factories\protocols\FactoryMachine
+class FactoryCreateRequest extends FactoryMachine
 {
     public function maker(): CreateRequest
     {
-        return new CreateRequest(new PHPServer());
+        $phpServer = new PHPServer();
+        $serverHTTPLoad = new ServerHTTPLoad();
+
+        $serverHTTPValidate = new ServerHTTPValidate(
+            $serverHTTPLoad,
+            $serverHTTPLoad,
+            $serverHTTPLoad
+        );
+
+        return new CreateRequest(
+            $phpServer,
+            $serverHTTPLoad,
+            $serverHTTPLoad,
+            $serverHTTPLoad,
+            $serverHTTPValidate,
+            $serverHTTPValidate,
+            $serverHTTPValidate,
+        );
     }
 }
