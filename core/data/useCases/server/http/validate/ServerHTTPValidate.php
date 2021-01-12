53779f14d4e747dbef054774761f13788c2c9bee
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-12 10:34:37 -0300

    refactor: implements functions ServerHTTPValidate

--- core/data/useCases/server/http/validate/ServerHTTPValidate.php
+++ core/data/useCases/server/http/validate/ServerHTTPValidate.php
@@ -6,22 +6,42 @@ namespace Core\data\useCases\server\http\validate;
 use Core\domain\protocols\server\http\validate\ServerHTTPValidateClientIP;
 use Core\domain\protocols\server\http\validate\ServerHTTPValidateRemoteAddress;
 use Core\domain\protocols\server\http\validate\ServerHTTPValidateXForwarder;
+use Core\domain\protocols\server\http\load\ServerHTTPLoadClientIP;
+use Core\domain\protocols\server\http\load\ServerHTTPLoadXForwarder;
+use Core\domain\protocols\server\http\load\ServerHTTPLoadRemoteAddress;
 
 class ServerHTTPValidate implements ServerHTTPValidateRemoteAddress, ServerHTTPValidateXForwarder, ServerHTTPValidateClientIP
 {
+    private $serverHTTPLoadClientIP;
+    private $serverHTTPLoadXForwarder;
+    private $serverHTTPLoadRemoteAddress;
+
+    public function __construct(
+        ServerHTTPLoadClientIP $serverHTTPLoadClientIP,
+        ServerHTTPLoadXForwarder $serverHTTPLoadXForwarder,
+        ServerHTTPLoadRemoteAddress $serverHTTPLoadRemoteAddress
+    )
+    {
+        $this->serverHTTPLoadClientIP = $serverHTTPLoadClientIP;
+        $this->serverHTTPLoadXForwarder = $serverHTTPLoadXForwarder;
+        $this->serverHTTPLoadRemoteAddress = $serverHTTPLoadRemoteAddress;
+    }
 
     public function validateHttpClientIPIsEmpty(): bool
     {
-        // TODO: Implement validateHttpClientIPIsEmpty() method.
+        if (empty($this->serverHTTPLoadClientIP->loadHttpClientIp())) return true;
+        return false;
     }
 
     public function validateHttpRemoteAddressIsEmpty(): bool
     {
-        // TODO: Implement validateHttpRemoteAddressIsEmpty() method.
+        if (empty($this->serverHTTPLoadRemoteAddress->loadHttpRemoteAddress())) return true;
+        return false;
     }
 
     public function validateHttpXForwarderIsEmpty(): bool
     {
-        // TODO: Implement validateHttpXForwarderIsEmpty() method.
+        if (empty($this->serverHTTPLoadXForwarder->loadHttpXForwarder())) return true;
+        return false;
     }
 }
