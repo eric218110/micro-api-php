ed9c28fc2726ccce4deb74b00ee800df88f75583
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-12 22:41:54 -0300

    refactor: use trait LLoadParamsRequest

--- core/data/useCases/http/request/create/CreateRequest.php
+++ core/data/useCases/http/request/create/CreateRequest.php
@@ -5,6 +5,7 @@ namespace Core\data\useCases\http\request\create;
 
 use Core\data\useCases\http\request\create\traits\CreateBodyRequest;
 use Core\data\useCases\http\request\create\traits\LoadClientIPRequest;
+use Core\data\useCases\http\request\create\traits\LoadParamsRequest;
 use Core\domain\protocols\http\request\create\CreateRequest as CreateRequestDomain;
 use Core\domain\protocols\server\http\load\ServerHTTPLoadClientIP;
 use Core\domain\protocols\server\http\load\ServerHTTPLoadRemoteAddress;
@@ -13,12 +14,18 @@ use Core\domain\protocols\server\http\validate\ServerHTTPValidateClientIP;
 use Core\domain\protocols\server\http\validate\ServerHTTPValidateRemoteAddress;
 use Core\domain\protocols\server\http\validate\ServerHTTPValidateXForwarder;
 use Core\domain\protocols\server\php\contents\ServerPHPGetContents;
+use Core\domain\protocols\http\uri\URILoad;
+use Core\domain\protocols\utils\path\RouterPath;
+use Core\domain\protocols\utils\strings\RemoveFirstAndLastParentheses;
+use Exception;
 use stdClass;
 
-class CreateRequest implements CreateRequestDomain {
+class CreateRequest implements CreateRequestDomain
+{
 
     use CreateBodyRequest;
     use LoadClientIPRequest;
+    use LoadParamsRequest;
 
     private $serverPHPGetContents;
     private $serverHTTPLoadClientIP;
@@ -27,6 +34,9 @@ class CreateRequest implements CreateRequestDomain {
     private $serverHTTPValidateClientIP;
     private $serverHTTPValidateRemoteAddress;
     private $serverHTTPValidateXForwarder;
+    private $routePath;
+    private $uriLoad;
+    private $removeFirstAndLastParentheses;
 
     public function __construct(
         ServerPHPGetContents $serverPHPGetContents,
@@ -35,7 +45,10 @@ class CreateRequest implements CreateRequestDomain {
         ServerHTTPLoadXForwarder $serverHTTPLoadXForwarder,
         ServerHTTPValidateClientIP $serverHTTPValidateClientIP,
         ServerHTTPValidateRemoteAddress $serverHTTPValidateRemoteAddress,
-        ServerHTTPValidateXForwarder $serverHTTPValidateXForwarder
+        ServerHTTPValidateXForwarder $serverHTTPValidateXForwarder,
+        RouterPath $routePath,
+        URILoad $uriLoad,
+        RemoveFirstAndLastParentheses $removeFirstAndLastParentheses
     )
     {
         $this->serverPHPGetContents = $serverPHPGetContents;
@@ -45,13 +58,11 @@ class CreateRequest implements CreateRequestDomain {
         $this->serverHTTPValidateClientIP = $serverHTTPValidateClientIP;
         $this->serverHTTPValidateRemoteAddress = $serverHTTPValidateRemoteAddress;
         $this->serverHTTPValidateXForwarder = $serverHTTPValidateXForwarder;
+        $this->routePath = $routePath;
+        $this->uriLoad = $uriLoad;
+        $this->removeFirstAndLastParentheses = $removeFirstAndLastParentheses;
     }
 
-    public function loadParams(): string
-    {
-        // TODO: Implement loadParams() method.
-
-    }
 
 
 
