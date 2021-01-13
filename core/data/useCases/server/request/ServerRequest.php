8254d34bb81217b27458255e430649fca7f0acc5
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-13 07:51:41 -0300

    refactor: use protocols ServerRequestBaseName

--- core/data/useCases/server/request/ServerRequest.php
+++ core/data/useCases/server/request/ServerRequest.php
@@ -2,11 +2,16 @@
 
 namespace Core\data\useCases\server\request;
 
+use Core\domain\protocols\server\request\ServerRequestBaseName;
 use Core\domain\protocols\server\request\ServerRequestHostName;
 use Core\domain\protocols\server\request\ServerRequestMethod;
 use Core\domain\protocols\server\request\ServerRequestURI;
 
-class ServerRequest implements ServerRequestHostName, ServerRequestURI, ServerRequestMethod
+class ServerRequest implements
+    ServerRequestHostName,
+    ServerRequestURI,
+    ServerRequestMethod,
+    ServerRequestBaseName
 {
     public function loadRequestURI(): string
     {
@@ -21,5 +26,10 @@ class ServerRequest implements ServerRequestHostName, ServerRequestURI, ServerRe
     public function loadRequestMethod(): string
     {
         return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function loadBaseNameURI(): string
+    {
+        return basename($_SERVER['REQUEST_URI']);
     }
 }
