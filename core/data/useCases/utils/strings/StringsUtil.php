99861303dd3320ed78cbaa226a9b3c4fab71c22b
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-12 22:42:57 -0300

    refactor: implements protocols remove first and last parentheses

--- core/data/useCases/utils/strings/StringsUtil.php
+++ core/data/useCases/utils/strings/StringsUtil.php
@@ -3,12 +3,20 @@
 namespace Core\data\useCases\utils\strings;
 
 use Core\domain\protocols\utils\strings\RemoveFirstAndLastBar;
+use Core\domain\protocols\utils\strings\RemoveFirstAndLastParentheses;
 
-class StringsUtil implements RemoveFirstAndLastBar
+class StringsUtil implements RemoveFirstAndLastBar, RemoveFirstAndLastParentheses
 {
     public function remove(string $valueString): string
     {
         $pattern = '/^\/?|\/?$/i';
+        $replacement = '${1}$3';
+        return preg_replace($pattern, $replacement, $valueString);
+    }
+
+    public function removeParentheses(string $valueString): string
+    {
+        $pattern = '/^\{?|\}?$/i';
         $replacement = '${1}$3';
         return preg_replace($pattern, $replacement, $valueString);
     }
