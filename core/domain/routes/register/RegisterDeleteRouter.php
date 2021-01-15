64c18dd28f69a35d4984a35cc158f94c37b9fe69
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-15 12:22:00 -0300

    feat: create protocols register methods HTTP

---
+++ core/domain/routes/register/RegisterDeleteRouter.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Core\domain\routes\register;
+
+
+interface RegisterDeleteRouter
+{
+    public function delete(string $path, callable $callbackFunction, array $args=[]): void;
+}
---
+++ core/domain/routes/register/RegisterRoutes.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Core\domain\routes\register;
+
+
+interface RegisterRoutes extends RegisterGetRouter
+{
+
+}
