cead2b62244eb258f212320d8b245bfaef545589
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-20 19:24:56 -0300

    refactor: implements trait CreateParamsTrait

--- core/data/useCases/http/request/create/traits/CreateParamsTrait.php
+++ core/data/useCases/http/request/create/traits/CreateParamsTrait.php
@@ -3,37 +3,35 @@
 
 namespace Core\data\useCases\http\request\create\traits;
 
+use stdClass;
 
 trait CreateParamsTrait
 {
     public function createParamsRequest(string $routeName): void
     {
-        $routesContents = $this->loadPathsRoute->loadPathRoute();
-//            $charExploderRoutes = '{';
-//
-//            foreach ($routesContents as $key => $routeContent) {
-//                if (strpos($key, $charExploderRoutes) !== false) {
-//                    $explodeInBars = '/';
-//                    $keyExploded = explode($explodeInBars, substr($key, 1));
-//                    $URIExploded = explode($explodeInBars, $this->uriLoad->loadURI());
-//
-//                    if (count($keyExploded) === count($URIExploded)) {
-//                        foreach ($keyExploded as $index => $item) {
-//                            if (strpos($item, $charExploderRoutes) === false) {
-//                                if ($URIExploded[$index] !== $item) {
-//                                    return new stdClass();
-//                                }
-//                            } else {
-//                                $valueWithoutParentheses = $this->removeFirstAndLastParentheses->removeParentheses($item);
-//                                $stdObj = new stdClass();
-//                                $stdObj->$valueWithoutParentheses = $URIExploded[$index];
-//                                return $stdObj;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//        return new stdClass();
+        {
+            $charExploderRoutes = '{';
+
+            if (strpos($routeName, $charExploderRoutes) !== false) {
+                $explodeInBars = '/';
+                $keyExploded = explode($explodeInBars, substr($routeName, 1));
+                $URIExploded = explode($explodeInBars, $this->uriLoad->loadURI());
+
+                if (count($keyExploded) === count($URIExploded)) {
+                    foreach ($keyExploded as $index => $item) {
+                        if (strpos($item, $charExploderRoutes) === false) {
+                            if ($URIExploded[$index] !== $item) {
+                                $this->request->setParams(new stdClass());
+                            }
+                        } else {
+                            $valueWithoutParentheses = $this->removeFirstAndLastParentheses->removeParentheses($item);
+                            $stdObj = new stdClass();
+                            $stdObj->$valueWithoutParentheses = $URIExploded[$index];
+                            $this->request->setParams($stdObj);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
