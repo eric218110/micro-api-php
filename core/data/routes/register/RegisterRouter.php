a8ef4406221b32887a9902425bca9cc27468b781
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-20 11:09:01 -0300

    refactor: change http method get RegisterRouter

--- core/data/routes/register/RegisterRouter.php
+++ core/data/routes/register/RegisterRouter.php
@@ -3,21 +3,50 @@
 
 namespace Core\data\routes\register;
 
+use Core\data\routes\register\traits\RegisterGetRouter;
 use Core\domain\routes\create\CreateRouter;
+use Core\domain\routes\match\RouteMatchWithHTTPMethod;
+use Core\domain\routes\match\RouteMatchWithURI;
 use Core\domain\routes\register\RegisterRoutes;
 
 class RegisterRouter implements RegisterRoutes
 {
-    private $routerCreator;
+    use RegisterGetRouter;
 
-    public function __construct(CreateRouter $createRouter)
+    private $routerCreator;
+    private $routeMatchWithURI;
+    private $routeMatchWithHTTPMethod;
+
+    const HTTP_METHOD_GET = 'get';
+
+    public function __construct(
+        CreateRouter $createRouter,
+        RouteMatchWithURI $routeMatchWithURI,
+        RouteMatchWithHTTPMethod $routeMatchWithHTTPMethod
+    )
     {
         $this->routerCreator = $createRouter;
+        $this->routeMatchWithURI = $routeMatchWithURI;
+        $this->routeMatchWithHTTPMethod = $routeMatchWithHTTPMethod;
     }
 
     public function get(string $path, callable $callbackFunction, array $args = []): void
     {
-        echo "ai";
-        $this->routerCreator->createRouter($path, $callbackFunction, $args);
+        if ($this->validateMatchRoute($path)) {
+            $this->routerCreator->createRouter($path, $callbackFunction, $args);
+
+        } else {
+            http_response_code(404);
+        }
+    }
+
+    private function validateMatchRoute(string $path): bool
+    {
+        if ($this->routeMatchWithURI->matchRouteWithURI($path)) {
+            if ($this->routeMatchWithHTTPMethod->matchRouteWithHTTPMethod(self::HTTP_METHOD_GET)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
