6b336e7c1eb7773d505763ba3113fa8bd8b158fd
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-20 20:01:40 -0300

    feat: create protocols CreateQueryTrait

--- core/data/useCases/http/request/create/traits/CreateQueryTrait.php
+++ core/data/useCases/http/request/create/traits/CreateQueryTrait.php
@@ -3,11 +3,29 @@
 
 namespace Core\data\useCases\http\request\create\traits;
 
+use stdClass;
 
 trait CreateQueryTrait
 {
     public function createQueryRequest(): void
     {
-        // TODO: Implement createQueryRequest() method.
+        $characterQuestionDot = "?";
+        $baseNameFromURI = $this->serverRequestBaseName->loadBaseNameURI();
+
+        $isQueryFormat = strpos($baseNameFromURI, $characterQuestionDot);
+
+        $stdObject = new stdClass();
+
+        if ($isQueryFormat) {
+            $explodeURI = explode($characterQuestionDot, $baseNameFromURI);
+            $queryValue = $explodeURI[1];
+            $characterEquals = '=';
+            $explodeQuery = explode($characterEquals, $queryValue);
+            $key = $explodeQuery[0];
+            $value = $explodeQuery[1];
+            $stdObject->$key = $value;
+        }
+
+        $this->request->setQuery($stdObject);
     }
 }
