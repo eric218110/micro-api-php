decfb35cea2a530cf48ba917358c64986fe0cfd7
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-20 19:17:54 -0300

    refactor: use CreateRequest protocol

--- core/data/routes/useCase/Route.php
+++ core/data/routes/useCase/Route.php
@@ -4,13 +4,11 @@
 namespace Core\data\routes\useCase;
 
 
-use Core\domain\protocols\http\request\create\CreateRequest;
 use Core\domain\routes\protocols\Route as RouteProtocols;
 
 class Route implements RouteProtocols
 {
     private $routes = array();
-    private $createRequest;
 
     const INDEX_PATH = 'path';
     const INDEX_CALLBACK_FUNCTION = 'callbackFunction';
