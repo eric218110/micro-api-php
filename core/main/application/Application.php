68d66b3a320683a467a47d37519ca91583a42e7b
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-20 22:02:53 -0300

    feat: create Application class

---
+++ core/main/application/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Core\main\application;
+
+use Core\domain\app\Application as ApplicationProtocols;
+use Core\main\factories\app\FactoryApplication;
+
+class Application
+{
+    private function __construct() {
+        
+    }
+
+    public static function maker(): ApplicationProtocols {
+        return (new FactoryApplication)->maker();
+    }
+}
