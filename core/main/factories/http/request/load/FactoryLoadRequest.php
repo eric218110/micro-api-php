ee3f537e4c3c88e86be02a436ce5205c11c2091d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-23 09:35:11 -0300

    feat: create factories FactoryLoadRequest, FactoryResponse, FactoryRegisterRouter

--- core/main/factories/http/request/load/FactoryLoadRequest.php
+++ core/main/factories/http/request/load/FactoryLoadRequest.php
@@ -3,8 +3,15 @@
 
 namespace Core\main\factories\http\request\load;
 
+use Core\data\useCases\http\request\load\LoadRequest;
+use Core\domain\app\HttpRequest;
+use Core\main\factories\http\request\FactoryRequest;
+use Core\main\factories\protocols\FactoryMachine;
 
-class FactoryLoadRequest
+class FactoryLoadRequest extends FactoryMachine
 {
-
+    public function maker(): HttpRequest
+    {
+        return new LoadRequest(FactoryRequest::getInstance());
+    }
 }
--- core/main/factories/http/response/FactoryResponse.php
+++ core/main/factories/http/response/FactoryResponse.php
@@ -4,7 +4,14 @@
 namespace Core\main\factories\http\response;
 
 
-class FactoryResponse
-{
+use Core\data\useCases\http\response\ResponseHttp;
+use Core\domain\app\HttpResponse;
+use Core\main\factories\protocols\FactoryMachine;
 
+class FactoryResponse extends FactoryMachine
+{
+    public function maker(): HttpResponse
+    {
+        return new ResponseHttp();
+    }
 }
