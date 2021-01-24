6d8bf4f8d96bd220de9d32b9e482fdd821b8dd0e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-24 18:50:30 -0300

    feat: create adapter controller to Callable function

--- src/main/adapter/controller/ControllerAdapter.php
+++ src/main/adapter/controller/ControllerAdapter.php
@@ -4,7 +4,14 @@
 namespace Src\main\adapter\controller;
 
 
+use Core\domain\app\controller\ApplicationController;
+
 class ControllerAdapter
 {
+    const INDEX_OF_METHOD_HANDLER = 0;
 
+    static function adapterControllerToCallable(ApplicationController $applicationController): array
+    {
+        return array($applicationController, get_class_methods($applicationController)[self::INDEX_OF_METHOD_HANDLER]);
+    }
 }
