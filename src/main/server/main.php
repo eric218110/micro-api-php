52a686822d8dd69c42ac1b38ad049c9f14418b83
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-24 18:51:34 -0300

    refactor: change main file

--- src/main/server/main.php
+++ src/main/server/main.php
@@ -1,13 +1,9 @@
 <?php
 
 use Core\main\application\Application;
+use Src\main\adapter\controller\ControllerAdapter;
 use Src\presentation\controller\home\HomeController;
 
 $app = Application::maker();
 
-$router = $app->router();
-
-$homeController = new HomeController();
-
-$router->get('/home', array($homeController, 'handler'));
-
+$app->router()->get('home', ControllerAdapter::adapterControllerToCallable(new HomeController()));
