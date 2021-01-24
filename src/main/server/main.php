8bbdf1d3d74fa4a5910a818f7beca94cb4ef7583
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-24 20:05:12 -0300

    feat: create and use home router

--- src/main/server/main.php
+++ src/main/server/main.php
@@ -1,9 +1,3 @@
 <?php
 
-use Core\main\application\Application;
-use Src\main\adapter\controller\ControllerAdapter;
-use Src\presentation\controller\home\HomeController;
-
-$app = Application::maker();
-
-$app->router()->get('home', ControllerAdapter::adapterControllerToCallable(new HomeController()));
+require_once __DIR__ . '/../routes/home-route.php';
