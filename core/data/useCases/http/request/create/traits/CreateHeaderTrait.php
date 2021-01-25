b6fff702982c24ef0c1bed885a92de57f1cc5d77
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-24 21:22:04 -0300

    refactor: implements protocols create header

--- core/data/useCases/http/request/create/traits/CreateHeaderTrait.php
+++ core/data/useCases/http/request/create/traits/CreateHeaderTrait.php
@@ -3,8 +3,17 @@
 
 namespace Core\data\useCases\http\request\create\traits;
 
+use stdClass;
 
 trait CreateHeaderTrait
 {
-
+    public function createHeader(): void
+    {
+        $headerFormat = new stdClass();
+        foreach ($this->serverApacheLoadHeadersRequest->loadHeaders() as $key => $value) {
+            $keyFormat = trim(str_replace('-', '_', $key));
+            $headerFormat->$keyFormat = $value;
+        }
+        $this->request->setHeaders($headerFormat);
+    }
 }
