d1d0fb6f0d146aeff4f632a29c7b1d922fae4f7f
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-26 08:59:54 -0300

    refactor: use FactoryValidateRouteMatch

--- core/main/factories/router/useCases/FactoryRegisterRouter.php
+++ core/main/factories/router/useCases/FactoryRegisterRouter.php
@@ -11,40 +11,36 @@ use Core\main\factories\http\request\load\FactoryLoadRequest;
 use Core\main\factories\http\response\FactoryResponse;
 use Core\main\factories\protocols\FactoryMachine;
 use Core\main\factories\router\FactoryRouter;
-use Core\main\factories\router\match\FactoryRouteMatchWithHTTPMethod;
-use Core\main\factories\router\match\FactoryRouteMatchWithURI;
+use Core\main\factories\router\validates\match\FactoryValidateRouteMatch;
 
 class FactoryRegisterRouter extends FactoryMachine
 {
-    private $routeMatchWithURI;
     private $routerRegister;
-    private $routeMatchWithHTTPMethod;
     private $createRequest;
     private $httpRequest;
     private $httpResponse;
     private $callResourceRoute;
+    private $validateMatchRoute;
 
     public function __construct()
     {
         $this->routerRegister = FactoryRouter::getInstance();
-        $this->routeMatchWithURI = (new FactoryRouteMatchWithURI())->maker();
-        $this->routeMatchWithHTTPMethod = (new FactoryRouteMatchWithHTTPMethod())->maker();
         $this->createRequest = (new FactoryCreateRequest())->maker();
         $this->httpRequest = (new FactoryLoadRequest())->maker();
         $this->httpResponse = (new FactoryResponse())->maker();
         $this->callResourceRoute = new RouteCallFunction();
+        $this->validateMatchRoute = (new FactoryValidateRouteMatch())->maker();
     }
 
     public function maker(): RegisterRoutes
     {
         return new RouterRegister(
             $this->routerRegister,
-            $this->routeMatchWithURI,
-            $this->routeMatchWithHTTPMethod,
             $this->createRequest,
             $this->httpRequest,
             $this->httpResponse,
-            $this->callResourceRoute
+            $this->callResourceRoute,
+            $this->validateMatchRoute
         );
     }
 }
