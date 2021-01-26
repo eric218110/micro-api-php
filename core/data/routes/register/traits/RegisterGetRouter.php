e83d5a844ddd8b5098f950e88b5157d7860fb5cf
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-01-26 09:01:24 -0300

    refactor: use function validateCreateRouterCreateBodyAndCallFunction

--- core/data/routes/register/traits/RegisterGetRouter.php
+++ core/data/routes/register/traits/RegisterGetRouter.php
@@ -6,5 +6,12 @@ namespace Core\data\routes\register\traits;
 
 trait RegisterGetRouter
 {
-
+    public function get(string $path, callable $callbackFunction, array $args = []): void
+    {
+        $this->validateCreateRouterCreateBodyAndCallFunction(
+            $path,
+            $callbackFunction,
+            $args,
+            self::HTTP_METHOD_GET);
+    }
 }
